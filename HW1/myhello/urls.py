a6003dba39f6655d92d2321d0a41c92a44874fc6
Nina Hsieh <ninahsieh950520@gmail.com>
2026-03-18 17:39:01 +0800

    upload ex0 ex1 ex2

---
+++ HW1/myhello/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # 符合要求的網址: 127.0.0.1:8000/myhello/courselist
+    path('courselist', views.list_course),
+    
+    # 符合要求的網址: 127.0.0.1:8000/myhello/addcourse
+    path('addcourse', views.add_course),
+]
---
+++ HW1/myhello/views.py
@@ -0,0 +1,28 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import logging
+from .models import Course  # 這裡只引入 Course
+
+logger = logging.getLogger('django')
+
+@api_view(['GET'])
+def add_course(request):
+    # 取得 URL 參數
+    dept = request.GET.get('Department', '')
+    title = request.GET.get('CourseTitle', '')
+    teacher = request.GET.get('Instructor', '')
+    
+    # 建立並儲存資料
+    new_c = Course(Department=dept, CourseTitle=title, Instructor=teacher)
+    new_c.save()
+    
+    logger.debug(f"*********** Course added: {title}")
+    return Response({"data": f"{title} insert!"}, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def list_course(request):
+    # 取得所有課程資料並回傳 JSON
+    courses = Course.objects.all().values()
+    return JsonResponse(list(courses), safe=False)
